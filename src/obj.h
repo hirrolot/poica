7b833251df0bee3d829f839cd697aed644948e59
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-06-05 12:33:31 +0600

    Add src/{obj.h, misc.h}, examples/obj.c

---
+++ src/obj.h
@@ -0,0 +1,19 @@
+#ifndef POICA_OBJ_H
+#define POICA_OBJ_H
+
+#include "misc.h"
+
+/**
+ * Expands to a pointer to an unnamed object of the specified type created
+ * in-place.
+ *
+ * @see https://en.cppreference.com/w/c/language/compound_literal
+ */
+#define OBJ(...) POICA_P_OBJ_AUX(__VA_ARGS__)
+
+#define POICA_P_OBJ_AUX(value, value_type)                                     \
+    (value_type *)(value_type[]) {                                             \
+        value                                                                  \
+    }
+
+#endif // POICA_OBJ_H
