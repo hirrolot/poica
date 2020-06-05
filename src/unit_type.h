d8f402fa7dcf1d054a997afd9c205598318f008c
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-06-05 20:55:39 +0600

    Use char in UnitType

--- src/unit_type.h
+++ src/unit_type.h
@@ -2,11 +2,11 @@
 #define POICA_UNIT_TYPE_H
 
 typedef struct UnitType {
-    bool poica_p_untouched;
+    char poica_p_untouched;
 } UnitType;
 
 inline static UnitType unit_type(void) {
-    return (UnitType){.poica_p_untouched = true};
+    return (UnitType){.poica_p_untouched = '\0'};
 }
 
 #endif // POICA_UNIT_TYPE_H
