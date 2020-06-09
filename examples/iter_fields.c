303cea021ed85c2bdbd70d215252611ce5c1d85e
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-06-10 00:18:02 +0600

    Simplify examples/iter_fields.c

--- examples/iter_fields.c
+++ examples/iter_fields.c
@@ -5,19 +5,14 @@
 
 #include <boost/preprocessor.hpp>
 
-#define ITER_FIELDS(macro, val, start_idx, end_idx)                            \
-    BOOST_PP_REPEAT_FROM_TO(start_idx, end_idx, INVOKE, (macro)(val))          \
+#define ITER_FIELDS(macro, val, count)                                         \
+    BOOST_PP_REPEAT(count, DISPLAY_FIELD, val)                                 \
     do {                                                                       \
     } while (false)
 
-#define INVOKE(_z, i, data) MACRO(data)(i, VAL(data))
-
-#define MACRO(data) BOOST_PP_SEQ_ELEM(0, data)
-#define VAL(data)   BOOST_PP_SEQ_ELEM(1, data)
-
-#define CONSUME(i, val)                                                        \
+#define DISPLAY_FIELD(_z, i, val)                                              \
     printf("tuple._%d = ", (i));                                               \
-    printf(SPECIFIER(BOOST_PP_CAT(val._, i)), BOOST_PP_CAT(val._, i));         \
+    printf(SPECIFIER(TUPLE_FIELD(val, i)), TUPLE_FIELD(val, i));               \
     puts("");
 
 #define SPECIFIER(val)                                                         \
@@ -37,5 +32,5 @@ int main(void) {
      * tuple._4 = 64
      * tuple._5 = 0x1383755
      */
-    ITER_FIELDS(CONSUME, tuple, 0, 6);
+    ITER_FIELDS(CONSUME, tuple, 6);
 }
