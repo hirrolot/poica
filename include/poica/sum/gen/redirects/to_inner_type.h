2d6f663aeb43d93fa482a912c1e4be85c179ecf8
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-06-14 01:47:05 +0600

    Decompose sum/gen/redirects/to_inner_type.h

--- include/poica/sum/gen/redirects/to_inner_type.h
+++ include/poica/sum/gen/redirects/to_inner_type.h
@@ -3,6 +3,10 @@
 
 #include <poica/private/aux.h>
 
+#include <poica/sum/gen/redirects/to_inner_type/variant_empty.h>
+#include <poica/sum/gen/redirects/to_inner_type/variant_many.h>
+#include <poica/sum/gen/redirects/to_inner_type/variant_single.h>
+
 #include <poica/sum/gen/variant_many.h>
 #include <poica/sum/introspection.h>
 #include <poica/sum/unused_field_type.h>
@@ -16,19 +20,6 @@
 #define POICA_P_SUM_GEN_REDIRECT_VARIANT_TO_INNER_TYPE(_r, _data, variant)     \
     OVERLOAD_ON_VARIANT(                                                       \
         POICA_P_SUM_GEN_REDIRECT_VARIANT_TO_INNER_TYPE_, _data, variant)
-
-#define POICA_P_SUM_GEN_REDIRECT_VARIANT_TO_INNER_TYPE_VARIANT_EMPTY(          \
-    _data, variant_name)                                                       \
-    typedef POICA_P_SUM_UnusedFieldType                                        \
-        POICA_P_SUM_REDIRECT_VARIANT_TO_INNER_TYPE(variant_name);
-
-#define POICA_P_SUM_GEN_REDIRECT_VARIANT_TO_INNER_TYPE_VARIANT_SINGLE(         \
-    _data, variant_name, variant_type)                                         \
-    typedef variant_type POICA_P_SUM_REDIRECT_VARIANT_TO_INNER_TYPE(           \
-        variant_name);
-
-#define POICA_P_SUM_GEN_REDIRECT_VARIANT_TO_INNER_TYPE_VARIANT_MANY(           \
-    _data, variant_name, _fields)
 
 #define POICA_P_SUM_REDIRECT_VARIANT_TO_INNER_TYPE(variant_name)               \
     POICA_P_PREFIX(BOOST_PP_CAT(variant_name, _RedirectToInnerType))
