ff46aa42dae377ae35d7e69c082accc610b21770
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-06-16 00:07:01 +0600

    Add comments

--- include/poica/sum/gen/redirects/to_inner_type/variant_empty.h
+++ include/poica/sum/gen/redirects/to_inner_type/variant_empty.h
@@ -3,7 +3,8 @@
 
 #include <boost/preprocessor.hpp>
 
+// Generate nothing because an empty variant has no actual type.
 #define POICA_P_SUM_GEN_REDIRECT_VARIANT_TO_INNER_TYPE_VARIANT_EMPTY(          \
-    _data, variant_name)
+    _data, _variant_name)
 
 #endif // SUM_GEN_REDIRECTS_TO_INNER_TYPE_VARIANT_EMPTY_H
--- include/poica/sum/gen/redirects/to_inner_type/variant_many.h
+++ include/poica/sum/gen/redirects/to_inner_type/variant_many.h
@@ -3,7 +3,10 @@
 
 #include <boost/preprocessor.hpp>
 
+// Generate nothing because we already generate a product type for
+// POICA_VARIANT_MANY with a name
+// POICA_P_SUM_REDIRECT_VARIANT_TO_INNER_TYPE(variant_name).
 #define POICA_P_SUM_GEN_REDIRECT_VARIANT_TO_INNER_TYPE_VARIANT_MANY(           \
-    _data, variant_name, _fields)
+    _data, _variant_name, _fields)
 
 #endif // SUM_GEN_REDIRECTS_TO_INNER_TYPE_VARIANT_MANY_H
