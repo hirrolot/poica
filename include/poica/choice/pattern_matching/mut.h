a5b6db323341abcf70c5af485521334fe0551b95
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-06-24 23:28:43 +0600

    Remove unused macros

--- include/poica/choice/pattern_matching/mut.h
+++ include/poica/choice/pattern_matching/mut.h
@@ -30,14 +30,12 @@
 
 #ifdef POICA_USE_PREFIX
 
-#define poicaMatchMut(choice_ptr) POICA_P_MATCH(, choice_ptr)
 #define poicaOfManyMut(variant_name, var_names)                                \
     POICA_P_OF_MANY(, variant_name, var_names)
 #define poicaOfMut(...) POICA_P_OF(, __VA_ARGS__)
 
 #else
 
-#define matchMut(choice_ptr) POICA_P_MATCH(, choice_ptr)
 #define ofManyMut(variant_name, var_names)                                     \
     POICA_P_OF_MANY(, variant_name, var_names)
 #define ofMut(...) POICA_P_OF(, __VA_ARGS__)
