628c453399ebebe92d4446c4068e298a4d450915
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-07-02 13:22:13 +0600

    Respect POICA_USE_PREFIX in include/poica/unit.h

--- include/poica/unit.h
+++ include/poica/unit.h
@@ -1,10 +1,22 @@
 #ifndef POICA_UNIT_H
 #define POICA_UNIT_H
 
+#ifdef POICA_USE_PREFIX
+
+typedef struct PoicaUnit {
+    char poica_p_untouched;
+} PoicaUnit;
+
+static const PoicaUnit poicaUnit;
+
+#else
+
 typedef struct Unit {
     char poica_p_untouched;
 } Unit;
 
 static const Unit unit;
+
+#endif // POICA_USE_PREFIX
 
 #endif // POICA_UNIT_H
