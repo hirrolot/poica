975ec99954ec93268a001d36da51bb0d9c54757d
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-07-13 18:02:15 +0600

    Simplify examples/int_bool_ast.c

--- examples/int_bool_ast.c
+++ examples/int_bool_ast.c
@@ -6,116 +6,92 @@
 
 #include <boost/preprocessor.hpp>
 
-// Because BOOST_PP_CAT becomes blueprinted.
-#define MY_CAT(a, b)     MY_CAT_AUX(a, b)
-#define MY_CAT_AUX(a, b) a##b
+#define ExprPair(expr_type)                                                    \
+    field(left, const struct expr_type *) field(right, const struct expr_type *)
 
-#define ExprPair(t1, t2)                                                       \
-    field(left, const struct P(Expr, t1, t2) *)                                \
-        field(right, const struct P(Expr, t1, t2) *)
+#define IExprPair ExprPair(Expr)
 
-#define IExprBody(type)                                                        \
-    P(Expr, int, type),                                                        \
-        variant(P(IConst, type), int)                                          \
-            variantMany(P(Add, type), ExprPair(int, type))                     \
-                variantMany(P(Sub, type), ExprPair(int, int))                  \
-                    variantMany(P(Mul, type), ExprPair(int, int))
+// clang-format off
+choice(
+    Expr,
+    variant(IConst, int)
+    variantMany(Add, IExprPair)
+    variantMany(Sub, IExprPair)
+    variantMany(Mul, IExprPair)
+);
+// clang-format on
 
-#define BExprBody(type)                                                        \
-    P(Expr, _Bool, type),                                                      \
+int evalExpr(const Expr *expr) {
+    match(*expr) {
+        of(IConst, number) {
+            return *number;
+        }
+        ofMany(Add, (left, right)) {
+            return evalExpr(*left) + evalExpr(*right);
+        }
+        ofMany(Sub, (left, right)) {
+            return evalExpr(*left) - evalExpr(*right);
+        }
+        ofMany(Mul, (left, right)) {
+            return evalExpr(*left) * evalExpr(*right);
+        }
+    }
+}
+
+// clang-format off
+#define DefBExpr(type)                                                         \
+    choice(                                                                    \
+        P(Expr, type),                                                         \
         variant(P(BConst, type), _Bool)                                        \
-            variantMany(P(And, type), ExprPair(_Bool, type))                   \
-                variantMany(P(Or, type), ExprPair(_Bool, type))                \
-                    variantMany(P(Xor, type), ExprPair(_Bool, type))           \
-                        variantMany(P(Eq, type), ExprPair(type, type))
+        variantMany(P(And, type), ExprPair(type))                              \
+        variantMany(P(Or, type), ExprPair(type))                               \
+        variantMany(P(Xor, type), ExprPair(type))                              \
+        variantMany(P(Eq, type), ExprPair(type))                               \
+    )
+// clang-format on
 
-choice(IExprBody(int));
-choice(IExprBody(_Bool));
-
-#define Exprintint_INTROSPECT   POICA_CHOICE_INTROSPECT(IExprBody(int))
-#define Exprint_Bool_INTROSPECT POICA_CHOICE_INTROSPECT(IExprBody(_Bool))
-
-choice(BExprBody(int));
-choice(BExprBody(_Bool));
-
-#define Expr_Boolint_INTROSPECT   POICA_CHOICE_INTROSPECT(BExprBody(int))
-#define Expr_Bool_Bool_INTROSPECT POICA_CHOICE_INTROSPECT(BExprBody(_Bool))
-
-#define declEval(t1, t2) t1 P(eval, t1, t2)(const P(Expr, t1, t2) * expr)
-
-#define defEval(t1, t2)                                                        \
-    POICA_P_EXPAND(declEval(t1, t2){match(*expr){BOOST_PP_SEQ_FOR_EACH(        \
-        GEN_CASE, (t1, t2), MY_CAT(P(Expr, t1, t2), _INTROSPECT))}})           \
-                                                                               \
-    POICA_FORCE_SEMICOLON
-
-#define GEN_CASE(_r, t1t2, variant)                                            \
-    BOOST_PP_CAT(GEN_CASE_, POICA_VARIANT_NAME(variant)) t1t2
-
-#define GEN_CASE_Add_Bool(t1, t2) GEN_CASE_AUX(P(Add, t1), +, int, int)
-#define GEN_CASE_Sub_Bool(t1, t2) GEN_CASE_AUX(P(Sub, t1), -, int, int)
-#define GEN_CASE_Mul_Bool(t1, t2) GEN_CASE_AUX(P(Mul, t2), *, int, int)
-
-#define GEN_CASE_Addint(t1, t2) GEN_CASE_Add_Bool(t1, t2)
-#define GEN_CASE_Subint(t1, t2) GEN_CASE_Sub_Bool(t1, t2)
-#define GEN_CASE_Mulint(t1, t2) GEN_CASE_Mul_Bool(t1, t2)
-
-#define GEN_CASE_And_Bool(t1, t2) GEN_CASE_AUX(P(And, t1), &&, _Bool, _Bool)
-#define GEN_CASE_Or_Bool(t1, t2)  GEN_CASE_AUX(P(Or, t1), ||, _Bool, _Bool)
-#define GEN_CASE_Xor_Bool(t1, t2) GEN_CASE_AUX(P(Xor, t1), ^, _Bool, _Bool)
-#define GEN_CASE_Eq_Bool(t1, t2)  GEN_CASE_AUX(P(Eq, t2), ==, t2, t2)
-
-#define GEN_CASE_Andint(t1, t2) GEN_CASE_And_Bool(t1, t2)
-#define GEN_CASE_Orint(t1, t2)  GEN_CASE_Or_Bool(t1, t2)
-#define GEN_CASE_Xorint(t1, t2) GEN_CASE_Xor_Bool(t1, t2)
-#define GEN_CASE_Eqint(t1, t2)  GEN_CASE_AUX(P(Eq, t2), ==, t2, t2)
-
-#define GEN_CASE_IConstint(t1, t2)                                             \
-    of(IConstint, val) {                                                       \
-        return *val;                                                           \
-    }
-#define GEN_CASE_IConst_Bool(t1, t2)                                           \
-    of(IConst_Bool, val) {                                                     \
-        return *val;                                                           \
+#define declEvalBExpr(type)                                                    \
+    _Bool P(eval, P(Expr, type))(const P(Expr, type) * expr)
+#define defEvalBExpr(type)                                                     \
+    declEvalBExpr(type) {                                                      \
+        match(*expr) {                                                         \
+            of(P(BConst, type), boolean) {                                     \
+                return *boolean;                                               \
+            }                                                                  \
+            ofMany(P(And, type), (left, right)) {                              \
+                return P(eval, type)(*left) && P(eval, type)(*right);          \
+            }                                                                  \
+            ofMany(P(Or, type), (left, right)) {                               \
+                return P(eval, type)(*left) || P(eval, type)(*right);          \
+            }                                                                  \
+            ofMany(P(Xor, type), (left, right)) {                              \
+                return P(eval, type)(*left) ^ P(eval, type)(*right);           \
+            }                                                                  \
+            ofMany(P(Eq, type), (left, right)) {                               \
+                return P(eval, type)(*left) == P(eval, type)(*right);          \
+            }                                                                  \
+        }                                                                      \
     }
 
-#define GEN_CASE_BConstint(t1, t2)                                             \
-    of(BConstint, val) {                                                       \
-        return *val;                                                           \
-    }
-#define GEN_CASE_BConst_Bool(t1, t2)                                           \
-    of(BConst_Bool, val) {                                                     \
-        return *val;                                                           \
-    }
+DefBExpr(Expr);
+DefBExpr(P(Expr, Expr));
 
-#define GEN_CASE_AUX(variant_name, op, t1, t2)                                 \
-    POICA_P_DEFER(ofMany)(variant_name, (left, right)) {                       \
-        return P(eval, t1, t2)(*left) op P(eval, t1, t2)(*right);              \
-    }
-
-declEval(int, _Bool);
-declEval(int, int);
-declEval(_Bool, _Bool);
-declEval(_Bool, int);
-
-defEval(int, _Bool);
-defEval(int, int);
-defEval(_Bool, _Bool);
-defEval(_Bool, int);
+defEvalBExpr(Expr);
+defEvalBExpr(P(Expr, Expr));
 
 int main(void) {
-    const P(Expr, _Bool, _Bool) true_node = P(BConst, _Bool)(true);
-    const P(Expr, _Bool, int) false_node = P(BConst, int)(false);
+    const P(Expr, Expr) true_node = P(BConst, Expr)(true);
+    const P(Expr, Expr) false_node = P(BConst, Expr)(false);
 
-    const P(Expr, int, int) _12_node = P(IConst, int)(12);
+    const Expr _12_node = IConst(12);
 
     // 12 == 12 == true
-    const P(Expr, _Bool, int) eq_node = P(Eq, int)(&_12_node, &_12_node);
+    const P(Expr, Expr) eq_node = P(Eq, Expr)(&_12_node, &_12_node);
 
     // true && false == false
-    P(Expr, _Bool, int) expr = P(And, int)(&eq_node, &false_node);
+    P(Expr, P(Expr, Expr)) expr = P(And, P(Expr, Expr))(&eq_node, &false_node);
 
-    _Bool res = P(eval, _Bool, int)(&expr);
+    _Bool res = P(eval, P(Expr, P(Expr, Expr)))(&expr);
 
     /*
      * Output:
