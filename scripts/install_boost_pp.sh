83d583ab5d71f3b5c86dee3a766a8c1f6031dbb0
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-08-12 06:57:13 +0600

    Add Boost PP & VMD as Git submodules

--- scripts/install_boost_pp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/boostorg/preprocessor/archive/boost-1.73.0.tar.gz
-tar -xvzf boost-1.73.0.tar.gz
-cd preprocessor-boost-1.73.0
-sudo cp include/boost /usr/include -rf
