952ada3b43cbb3368786c7be62094d2ad84b432e
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-08-21 01:39:31 +0600

    Nicer syntax for interfaces

--- include/poica/lang/interface.h
+++ include/poica/lang/interface.h
@@ -27,6 +27,7 @@
 #define POICA_LANG_INTERFACE_H
 
 #include <poica/lang/force_semicolon.h>
+#include <poica/lang/private/consume.h>
 #include <poica/lang/private/prefix.h>
 
 #include <boost/preprocessor.hpp>
@@ -54,6 +55,10 @@
 
 #endif
 
+#define BOOST_VMD_REGISTER_for (for)
+
+#define BOOST_VMD_DETECT_for_for
+
 #define POICA_P_LANG_INTERFACE(name, methods)                                  \
     typedef struct POICA_P_LANG_VTABLE(name) {                                 \
         methods                                                                \
@@ -72,27 +77,51 @@
     POICA_FORCE_SEMICOLON
 
 #define POICA_P_LANG_IMPL(...)                                                 \
-    BOOST_PP_OVERLOAD(POICA_P_LANG_IMPL_, __VA_ARGS__)(__VA_ARGS__)
+    BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), 1),        \
+                POICA_P_LANG_IMPL_1,                                           \
+                POICA_P_LANG_IMPL_2)                                           \
+    (__VA_ARGS__)
 
-#define POICA_P_LANG_IMPL_2(interface_name, implementer_name)                  \
+#define POICA_P_LANG_IMPL_1(data)                                              \
+    POICA_P_LANG_IMPL_1_AUX(POICA_P_LANG_EXPAND BOOST_VMD_ELEM(0, data),       \
+                            POICA_P_LANG_EXPAND BOOST_VMD_ELEM(2, data))
+
+#define POICA_P_LANG_IMPL_1_AUX(interface_name, implementer_name)              \
     const POICA_P_LANG_VTABLE(interface_name)                                  \
         POICA_P_LANG_VTABLE(interface_name, implementer_name);
 
-#define POICA_P_LANG_IMPL_3(interface_name, implementer_name, methods)         \
-    POICA_P_LANG_INTERFACE_IMPL_AUX(                                           \
-        static, interface_name, implementer_name, methods)
+#define POICA_P_LANG_IMPL_2(data, ...)                                         \
+    POICA_P_LANG_IMPL_2_AUX(POICA_P_LANG_EXPAND BOOST_VMD_ELEM(0, data),       \
+                            POICA_P_LANG_EXPAND BOOST_VMD_ELEM(2, data),       \
+                            __VA_ARGS__)
 
-#define POICA_P_LANG_STATIC_IMPL_3(interface_name, implementer_name, methods)  \
+#define POICA_P_LANG_IMPL_2_AUX(interface_name, implementer_name, ...)         \
     POICA_P_LANG_INTERFACE_IMPL_AUX(                                           \
-        BOOST_PP_EMPTY(), interface_name, implementer_name, methods)
+        static, interface_name, implementer_name, __VA_ARGS__)
 
-#define POICA_P_LANG_INTERFACE_IMPL_AUX(vtable_qualifiers_and_specifiers,      \
-                                        interface_name,                        \
-                                        implementer_name,                      \
-                                        methods)                               \
+#define POICA_P_LANG_STATIC_IMPL(data, methods)                                \
+    POICA_P_LANG_STATIC_IMPL_AUX(POICA_P_LANG_EXPAND BOOST_VMD_ELEM(0, data),  \
+                                 POICA_P_LANG_EXPAND BOOST_VMD_ELEM(2, data),  \
+                                 methods)
+
+#define POICA_P_LANG_STATIC_IMPL_AUX(interface_name, implementer_name, ...)    \
+    POICA_P_LANG_INTERFACE_IMPL_AUX(                                           \
+        BOOST_PP_EMPTY(), interface_name, implementer_name, __VA_ARGS__)
+
+#define POICA_P_LANG_INTERFACE_IMPL_AUX(                                       \
+    vtable_qualifiers_and_specifiers, interface_name, implementer_name, ...)   \
+    POICA_P_LANG_INTERFACE_IMPL_AUX2(vtable_qualifiers_and_specifiers, interface_name, implementer_name,POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS(__VA_ARGS__))
+
+#define POICA_P_LANG_INTERFACE_IMPL_AUX2(vtable_qualifiers_and_specifiers,     \
+                                         interface_name,                       \
+                                         implementer_name,                     \
+                                         methods)                              \
+    POICA_P_LANG_IMPL_GEN_METHODS(interface_name, implementer_name, methods)   \
+                                                                               \
     const vtable_qualifiers_and_specifiers POICA_P_LANG_VTABLE(interface_name) \
         POICA_P_LANG_VTABLE(interface_name, implementer_name) = {              \
-            POICA_P_LANG_EXPAND(POICA_P_LANG_EXPAND methods)};                 \
+            POICA_P_LANG_IMPL_ENUM_METHOD_NAMES(                               \
+                interface_name, implementer_name, methods)};                   \
                                                                                \
     POICA_P_LANG_INTERFACE_DEF_NEW_I_OBJ(                                      \
         interface_name,                                                        \
@@ -106,6 +135,102 @@
         BOOST_PP_EMPTY())                                                      \
                                                                                \
     POICA_FORCE_SEMICOLON
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS(...)                       \
+    BOOST_PP_SEQ_FOR_EACH(POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_VISIT,   \
+                          BOOST_PP_EMPTY(),                                    \
+                          BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_VISIT(_r, _data, method)   \
+   ((BOOST_PP_SEQ_ELEM(0, method)) (BOOST_PP_SEQ_ELEM(1, method)) POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_PARENTHESISE_PARAMS(method) POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_PARENTHESISE_BODY(method))
+
+
+
+
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_PARENTHESISE_PARAMS(       \
+    method)                                                                    \
+       (POICA_P_LANG_EXPAND(POICA_P_LANG_EXPAND(   POICA_P_LANG_CONSUME POICA_P_LANG_CONSUME POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST(method))))
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_PARENTHESISE_PARAMS_AUX1(  \
+    ...)                                                                       \
+    (__VA_ARGS__)
+
+
+
+
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST(data) POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_AUX(POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_K, data)
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_AUX(k, data) k(POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_PICK_WITH_COMMA data)
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_K(...) POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_K_AUX(__VA_ARGS__) 
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_K_AUX(x, y) (x)POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST2(y)
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST2(data) POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST2_AUX(POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST2_K, data)
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST2_AUX(k, data) k(POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_PICK_WITH_COMMA data)
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST2_K(...) POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST2_K_AUX(__VA_ARGS__) 
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST2_K_AUX(x, y) (x)POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST3(y)
+
+
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST3(data) POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST3_AUX(POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST3_K, data)
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST3_AUX(k, data) k(POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_PICK_WITH_COMMA data)
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST3_K(...) POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST3_K_AUX(__VA_ARGS__) 
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST3_K_AUX(x, y) (x)
+
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_WITHOUT_LAST_PICK_WITH_COMMA(first, ...) first,
+
+
+
+#define POICA_P_LANG_INTERFACE_IMPL_PREPARE_METHODS_PARENTHESISE_BODY(method)  \
+                                                                               \
+    (POICA_P_LANG_EXPAND(POICA_P_LANG_EXPAND(        \
+        POICA_P_LANG_CONSUME POICA_P_LANG_CONSUME POICA_P_LANG_CONSUME method)))
+
+
+#define POICA_P_LANG_IMPL_GEN_METHODS(                                         \
+    interface_name, implementer_name, methods)                                 \
+    BOOST_PP_SEQ_FOR_EACH_I(POICA_P_LANG_IMPL_GEN_METHODS_VISIT,               \
+                            (interface_name)(implementer_name),                \
+                            methods)
+
+#define POICA_P_LANG_IMPL_GEN_METHODS_VISIT(_r, data, i, method)               \
+    static POICA_P_LANG_IMPL_METHOD_RETURN_TYPE(method)                        \
+        POICA_P_LANG_IMPL_METHOD_NAME(                                         \
+            i, method, BOOST_PP_SEQ_ELEM(0, data), BOOST_PP_SEQ_ELEM(1, data)) \
+            POICA_P_LANG_IMPL_METHOD_PARAMS(method)                            \
+                POICA_P_LANG_IMPL_METHOD_BODY(method)
+
+#define POICA_P_LANG_IMPL_ENUM_METHOD_NAMES(                                   \
+    interface_name, implementer_name, methods)                                 \
+    BOOST_PP_SEQ_FOR_EACH_I(POICA_P_LANG_IMPL_ENUM_METHODS_VISIT,              \
+                            (interface_name)(implementer_name),                \
+                            methods)
+
+#define POICA_P_LANG_IMPL_ENUM_METHODS_VISIT(_r, data, i, method)              \
+    POICA_P_LANG_IMPL_METHOD_NAME(                                             \
+        i, method, BOOST_PP_SEQ_ELEM(0, data), BOOST_PP_SEQ_ELEM(1, data)),
+
+#define POICA_P_LANG_IMPL_METHOD_NAME(                                         \
+    i, method, interface_name, implementer_name)                               \
+    POICA_P_LANG_PREFIX(                                                       \
+        BOOST_PP_CAT(BOOST_PP_CAT(interface_name,                              \
+                                  BOOST_PP_CAT(implementer_name,               \
+                                               BOOST_PP_SEQ_ELEM(1, method))), \
+                     i))
+#define POICA_P_LANG_IMPL_METHOD_RETURN_TYPE(method)                           \
+    BOOST_PP_SEQ_ELEM(0, method)
+#define POICA_P_LANG_IMPL_METHOD_PARAMS(method) BOOST_PP_SEQ_ELEM(2, method)
+#define POICA_P_LANG_IMPL_METHOD_BODY(method)                                  \
+    {                                                                          \
+        POICA_P_LANG_IMPL_METHOD_BODY_EXPAND(                                  \
+            POICA_P_LANG_EXPAND BOOST_PP_SEQ_ELEM(3, method))                  \
+    }
+
+#define POICA_P_LANG_IMPL_METHOD_BODY_EXPAND(...) __VA_ARGS__
 
 #define POICA_P_LANG_VTABLE(...)                                               \
     BOOST_PP_OVERLOAD(POICA_P_LANG_VTABLE_, __VA_ARGS__)(__VA_ARGS__)
